4a8e469bc2efd1d388ffe79ae916cdda53b2d6e7
Pfmello <philipmikh@hotmail.com>
2023-03-16 15:35:37 -0300

    Vetor criado

--- DesafioMatriz/Program.cs
+++ DesafioMatriz/Program.cs
@@ -12,15 +12,33 @@ namespace DesafioMatriz
         {
             "Desafio De Fixação Matriz !".Print();
 
-            int M = "Insira o número de LINHAS da matriz:".ReturnInt();
-            int N = "Insira o número de COLUNAS da matriz:".ReturnInt();
+            int linha = "Insira o número de LINHAS da matriz:".ReturnInt();
+            int coluna = "Insira o número de COLUNAS da matriz:".ReturnInt();
+            int[,] matriz = new int[linha, coluna];
 
-            int[,] matriz = new int[M, N];
+            PreencherMatriz(linha, coluna, matriz);
 
-            PercorrerMatriz(matriz, N);
+            PercorrerMatriz(matriz, coluna);
 
 
             Console.ReadLine();
+        }
+
+        private static void PreencherMatriz(int linha, int coluna, int[,] matriz)
+        {
+
+            string numeros = "10 8 15 12 21 11 23 8 14 5 13 19 8";
+            string[] vetor = numeros.Split(' ');
+            int indexVetor = 0;
+
+            for (int i = 0; i < linha; i++)
+            {
+                for (int j = 0; j < coluna; j++)
+                {
+                    matriz[i, j] = int.Parse(vetor[indexVetor]);
+                    indexVetor++;
+                }
+            }
         }
 
         private static void PercorrerMatriz(int[,] matriz, int N)
